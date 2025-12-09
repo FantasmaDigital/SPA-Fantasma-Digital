846122c8effa3f39f00cdbe14c9cb525aba52836
fantasmadigitalsv <fantasmadigitalsv@gmail.com>
2025-12-09 17:26:29 -0600

    add: projects section

---
+++ src/constants/header.ts
@@ -0,0 +1,12 @@
+type HeaderLink = {
+    title: string;
+    href: string;
+}
+
+export const headerLinks: HeaderLink[] = [
+    { title: "Inicio", href: "/" },
+    { title: "Servicios", href: "/servicios" },
+    { title: "Proyectos", href: "/proyectos" },
+    { title: "Nosotros", href: "/nosotros" },
+    { title: "Contacto", href: "/contacto" },
+]
